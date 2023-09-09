74d390647c45c846ba9bfd87e94bd1706ec87287
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-09 16:57:44 -0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by João Victor de Souza Guedes on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
